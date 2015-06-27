7d78e25598d775a2449c2e8f1543e21097ae5b99
Ryan <hiphubryan@gmail.com>
2015-06-27 13:24:58 +0800

    Finish register form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   root to: "pages#index"
+  resources :users, only: [:new, :create, :edit, :update]
+
+  resources :sessions, only: :create
+  get 'sign_in', to: "sessions#new"
+  delete 'sign_out', to: "sessions#destroy"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
