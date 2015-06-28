ec46b6b6c0b5a7a20ea79007cb9ac58ded6cdd8f
Ryan <hiphubryan@gmail.com>
2015-06-29 00:34:56 +0800

    Remove turbolink

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,9 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-// require bootstrap-sprockets
-// require bootstrap-multiselect
-//= require bootstrap.min.js
+//= require bootstrap-sprockets
+//= require bootstrap-multiselect
 //= require jquery.easing.min.js
 //= require jquery.fittext.js
 //= require wow.min.js
