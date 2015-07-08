8c1f5d439cee3bdd15fbd339ec8972aabfb95740
Ryan <hiphubryan@gmail.com>
2015-07-08 12:50:06 +0800

    Modify app_mailer

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -1,14 +1,14 @@
 class AppMailer < ActionMailer::Base
-  default from: "girlstimegoodtime @gmail.com"
+  default from: "girlstimegoodtime@gmail.com"
 
   def send_welcome_email(user)
     @user = user
-    mail to: user.email, subject: "【 註冊成功】好時光歡迎妳的加入"
+    mail to: user.email, subject: "【註冊成功】好時光歡迎妳的加入"
   end
 
   def send_forgot_password(user)
     @user = user
-    mail to: user.email, subject: "【好時光密碼重設通知"
+    mail to: user.email, subject: "【好時光密碼重設通知】"
   end
 
   # def send_invitation_email(invitation)
